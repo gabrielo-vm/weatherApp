ef11732ec4e6cbaba96b13579b4fa72dab75b70a
Gabriel Valdez Mendoza <valdez.mendoza@me.com>
2024-10-03 12:38:01 -0700

    Initiation of weather app

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gabriel Valdez Mendoza on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
